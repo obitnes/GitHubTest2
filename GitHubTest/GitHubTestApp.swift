11f69eff027ca0d4991d6e59f964c763a8480d6e
Ole <olebitnes@gmail.com>
2023-04-23 21:15:50 +0200

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Default User on 23/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
